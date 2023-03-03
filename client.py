adffe16a82f356c4f847e8d5e24e1f42935722bc
ปุญญพัฒน์ แก้วขาว <aimaster0010@gmail.com>
2023-03-03 15:58:09 +0700

    Create class for a game function + Edit client to play the game in the class after the connection

--- client.py
+++ client.py
@@ -1,10 +1,17 @@
 import socket
 
-
 HOST = "127.0.0.1"
 PORT = 8888
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
+
+while True:
+    guess = input("Guess the number between 1 and 100: ")
+    client_socket.send(guess.encode())
+    hint = client_socket.recv(1024).decode()
+    print(hint)
+    if "Congratulations" in hint or "Sorry" in hint:
+        break
 
 client_socket.close()
--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import socket
-
+import random 
 
 HOST = "127.0.0.1"
 PORT = 8888
@@ -11,6 +11,32 @@ server_socket.listen()
 
 print("Server started. Waiting for connections...")
 
+class NumberGuessingGame:
+    def __init__(self):
+        self.number = None
+        self.attempts = 0
+
+    def play_game(self, client):
+        self.number = random.randint(1, 100)
+        self.attempts = 0
+        print("Client connected:", client_address)
+        while self.attempts < 10:
+            guess = client.recv(1024).decode()
+            print("Client {} says: {}".format(client.getpeername(), guess))
+            if guess == str(self.number):
+                client.send("Congratulations! You guessed the number in {} attempts!".format(self.attempts+1).encode())
+                return
+            else:
+                self.attempts += 1
+                if self.attempts < 10:
+                    if int(guess) < self.number:
+                        client.send("Try a higher number. You have {} attempts left.".format(10-self.attempts).encode())
+                    else:
+                        client.send("Try a lower number. You have {} attempts left.".format(10-self.attempts).encode())
+                else:
+                    client.send("Sorry, you have run out of attempts. The number was {}.".format(self.number).encode())
+
 while True:
     client_socket, client_address = server_socket.accept()
-    print("Client connected:", client_address)
+    game = NumberGuessingGame()
+    game.play_game(client_socket)
