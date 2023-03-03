aed39e68d3085949f336a561da23b89d5d415f3a
Natcha Laepankaew <6510110132@psu.ac.th>
2023-03-03 20:39:15 +0700

    Edit the condition for the Invalid input

--- client.py
+++ client.py
@@ -7,11 +7,15 @@ client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((HOST, PORT))
 
 while True:
-    guess = input("Guess the number between 1 and 100: ")
-    client_socket.send(guess.encode())
-    hint = client_socket.recv(1024).decode()
-    print(hint)
-    if "Congratulations" in hint or "Sorry" in hint:
-        break
+    try:
+        guess = int(input("Guess the number between 1 and 100: "))
+        guess = str(guess)
+        client_socket.send(guess.encode())
+        hint = client_socket.recv(1024).decode()
+        print(hint)
+        if "Congratulations" in hint or "Sorry" in hint:
+            break
+    except:
+        print('Invalid input')
 
 client_socket.close()
